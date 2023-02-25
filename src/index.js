93b0b45195a6271b2f82aa8f0b1ee9ab2510a530
Matee <matiullahsafibbc@gmail.com>
2023-02-24 20:35:20 -0600

    Add list items with js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-import './style.css';
-import reloadIcon from './refresh.png';
-import enterIcon from './enter.png';
-import moreIcon from './more.png';
-
-const refresh = document.querySelector('.refresh');
-refresh.src = reloadIcon;
-const enter = document.querySelector('.enter');
-enter.src = enterIcon;
-const more = document.querySelectorAll('.more');
-more.forEach((img) => {
-  img.src = moreIcon;
-});
+import "./style.css";
+import reloadIcon from "./images/refresh.png";
+import enterIcon from "./images/enter.png";
+import moreIcon from "./images/more.png";
 
 const tasks = [
   {
-    description: '',
+    description: "Create the structure of your project",
     completed: false,
     index: 0,
   },
 ];
 
 tasks.forEach((task) => {
-  const list = document.createElement('div');
-  list.classList.add('list-item');
+  const list = document.createElement("div");
+  list.classList.add("list-item");
   list.innerHTML = `
-  <input type="checkbox">
-  <p>${task.description}</p>
-  <img src="" alt="">
+  <li class="todo">
+    <div>
+        <input type="checkbox"><p>${task.description}</p>
+    </div>
+    <img alt="more" class="more">
+  </li>
   `;
+  document.querySelector('.last-row').insertAdjacentElement('beforebegin', list)
 });
+
+const refresh = document.querySelector(".refresh");
+refresh.src = reloadIcon;
+const enter = document.querySelector(".enter");
+enter.src = enterIcon;
+const more = document.querySelectorAll(".more");
+more.forEach((img) => {
+  img.src = moreIcon;
+});
+
